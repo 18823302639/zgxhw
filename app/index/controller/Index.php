aa48dbc28bdcb716cb00e85a56e38dbbfe97b32e
xiaokxuan <2275415324@qq.com>
2018-12-10 10:57:34 +0800

    修改：把图片从跟踪列表中删除

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -28,9 +28,15 @@ class Index extends Controller
         return $this->fetch();
     }
 
+    //香港公司年审
+    public function review(){
+        return $this->fetch();
+    }
+
     public function aa(){
          $a = '1'; $b = "1998"; $c = 19; echo $a+$b+$c;
     }
+
     public function cs(){
         var_dump($GLOBALS);
         return $this->fetch();
